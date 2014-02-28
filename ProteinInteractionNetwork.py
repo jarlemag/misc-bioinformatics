be308281eb54c7f3dba7c9c8e1e6edbe5d015a09
Jarle Pahr <jarle.pahr@gmail.com>
2014-02-28 16:19:42 +0100

    Wu 2010 network.

--- ProteinInteractionNetwork.py
+++ ProteinInteractionNetwork.py
@@ -8,7 +8,7 @@ import numpy as np
 import random
 
 class ProteinInteractionNetwork(object):
-    def readInteractions(self,filename,fraction = 1, verbose = True):
+    def readInteractions(self,filename,fraction = 1, verbose = True,delimiter = 'pp'):
         with open(filename, 'r') as sourcefile:
             if verbose:
                 print 'Reading interactions from',filename
@@ -17,14 +17,15 @@ class ProteinInteractionNetwork(object):
                 #Select a random subset of the network
             else:
                 for line in sourcefile:
-                    self.interactions.append(tuple([p.strip() for p in line.split('pp')]))
+                    self.interactions.append(tuple([p.strip() for p in line.split(delimiter)]))
            
-    def __init__(self,SIF_file):
+    def __init__(self,SIF_file,description = None,size_limit=None,delimiter = 'pp'):
         self.interactions = []
         self.sequencelengths = {}
         self.self_interactions = 0
-        self.readInteractions(SIF_file)
-        self.proteins =  set(list(itertools.chain.from_iterable(self.interactions)))
+        self.readInteractions(SIF_file,delimiter  = delimiter)
+        self.proteins = set(list(itertools.chain.from_iterable(self.interactions)))
+        self.description = description
     def showprogress(self,start_time,target,processed):
         elapsed_time = round(time.time() - start_time,2)
         print 'Processed:',processed,'Elapsed time (s):',elapsed_time
@@ -35,10 +36,10 @@ class ProteinInteractionNetwork(object):
             print 'Estimated time left (min):',round(remaining*time_per_entry/60,1)
 
             
-    def downloadSequenceLengths(self,limit = None, verbose = True,filename = None):
-        if filename is not None: #If a file (containing previously retrieved sequence lengths) is specified
-            if os.path.exists(filename): #If the output file exists
-                with open(filename,'r') as sourcefile:
+    def downloadSequenceLengths(self,limit = None, verbose = True,file_in = None,file_out = None):
+        if file_in is not None: #If a file (containing previously retrieved sequence lengths) is specified
+            if os.path.exists(file_in): #If the output file exists
+                with open(file_in,'r') as sourcefile:
                     retrieved = [line.strip().split(' ')[1] for line in sourcefile]
                 for protein in self.proteins:
                     if protein not in retrieved:
@@ -61,7 +62,7 @@ class ProteinInteractionNetwork(object):
                 self.failedretrieval.append(protein_id)
                 if verbose:
                     print 'Failed to access xml for protein',protein_id,'at',xmlurl
-                    showprogress(start_time,len(self.proteins),processed)
+                    self.showprogress(start_time,len(self.proteins),processed)
                 continue
             try:
                 tree = ET.parse(xml)
@@ -69,7 +70,7 @@ class ProteinInteractionNetwork(object):
                 if verbose:
                     print 'Failed to parse xml for protein',protein_id
                     print 'xml URL:',xmlurl
-                    showprogress(start_time,len(self.proteins),processed)
+                    self.showprogress(start_time,len(self.proteins),processed)
                 continue
             root = tree.getroot()
             try:
@@ -77,7 +78,14 @@ class ProteinInteractionNetwork(object):
             except:
                 self.nosequence.append(protein_id)
                 continue
-            self.sequencelengths[protein_id] = AA_sequence.attrib['length']
+            length = AA_sequence.attrib['length']
+            self.sequencelengths[protein_id] = int(length)
+            if file_out is not None:
+                with open(file_out,'a') as target:
+                    target.write(protein_id)
+                    target.write(' ')
+                    target.write(length)
+                    target.write('\n')
             processed +=1
     def readSequenceLengths(self,filename,replace = True):
         if replace:
@@ -158,6 +166,13 @@ class ProteinInteractionNetwork(object):
                     if processed % 1000 == 0:
                       self.showprogress(start_time,interaction_number,processed)
 
+    def randomizeSequenceLengths(self):
+        n = len(self.sequencelengths)
+        centre = np.median(self.sequencelengths.values())
+        randomvalues = list(np.random.normal(centre,size=n))
+        randomvalues = [int(value) for value in randomvalues]
+        for key in self.sequencelengths:
+            self.sequencelengths[key] = randomvalues.pop(random.randrange(len(randomvalues)))
     def writeInteractions(self,filename):
         with open(filename,'w') as target:
             for interaction in self.interactions:
@@ -171,14 +186,36 @@ class ProteinInteractionNetwork(object):
         percentile = np.percentile(proportions,percentile_cutoff)
         fig = plt.figure()
         plt.hist(proportions,bins = 20,range=(1,percentile),normed = normalize)
-        plt.xlabel('Relative protein size')
+        plt.xlabel('Relative protein chain length')
         plt.ylabel('Fraction of interacting protein pairs')
+        plt.title('Relative chain lengths among interacting protein pairs')
         plt.show()
-        
+
+    def sequenceLengthsHistogram(self,percentile_cutoff = 99,normalize = True,bins=20):
+        fig = plt.figure()
+        lengths = self.sequencelengths.values()
+        percentile = np.percentile(lengths,percentile_cutoff)
+        plt.hist(lengths,bins = bins,range=(1,percentile),normed=normalize)
+        plt.xlabel('Protein chain length')
+        plt.ylabel('Fraction of proteins')
+        plt.title('Protein chain length distribution')
+        plt.show()
+    
 if __name__ == "__main__":
     PIN = ProteinInteractionNetwork('Homo sapiens-20121210.sif')
     #PIN.downloadSequenceLengths() #Takes about an hour for 15 000 proteins
     PIN.readSequenceLengths('protlengths.txt')
     PIN.sequenceProportionHistogram()
-    PIN.randomizeInteractions()
+    #PIN.randomizeInteractions()
     #PIN.sequenceProportionHistogram()
+    #yeastPIN = ProteinInteractionNetwork('Saccharomyces cerevisiae-20121210.sif')
+    #yeastPIN.downloadSequenceLengths(file_out = 'yeastprotlengths.txt') #Takes about 20 minutes for 6000 proteins
+    #yeastPIN.readSequenceLengths('yeastprotlengths.txt')
+    #elegansPIN = ProteinInteractionNetwork('Caenorhabditis elegans-20121210.sif')
+    #elegansPIN.downloadSequenceLengths(file_out = 'elegansprotlengths.txt')
+    #elegansPIN.readSequenceLengths('elegansprotlengths.txt')
+    Wu2010PIN = ProteinInteractionNetwork('FIs_043009.txt','Human functional protein interaction network (Wu 2010)',delimiter=' ')
+    #Wu2010PIN.downloadSequenceLengths(file_out = 'Wu2010protlengths.txt')
+    Wu2010PIN.readSequenceLengths('Wu2010protlengths.txt')
+    Wu2010PIN.sequenceLengthsHistogram()
+    Wu2010PIN.sequenceProportionHistogram()
